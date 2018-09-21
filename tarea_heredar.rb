ecc17e77aeeb76a8ccd6268e4fedb07acfd8c671
JonathanVizcarra <josavizcarraca@ittepic.edu.mx>
2018-09-21 15:47:31 +0000

    feat: add tarea_heredar

---
+++ tarea_heredar.rb
@@ -0,0 +1,19 @@
+class Documentos
+    attr_accessor :nombre_doc, :paginas, :precio
+end
+
+class Libro < Documentos
+    attr_accessor :autor
+    def initialize nombre_doc:, paginas:, precio:, autor:
+        @nombre_doc = nombre_doc
+        @paginas = paginas
+        @precio = precio
+        @autor = autor
+    end
+    def to_s
+        "Libro: #{nombre_doc}, Autor: #{autor}, Paginas: #{paginas}, Precio: $#{precio}"
+    end
+end
+
+libro01 = Libro.new(nombre_doc:"Pro Git",autor:"Scott Chacon y Ben Straub",paginas:506,precio:350)
+puts libro01
